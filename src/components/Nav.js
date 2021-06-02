72828d742e80ed2f69794c2c4df0cc7e0a888a04
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-02 14:32:30 +0200

    components -> Nav.js -> content

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,25 @@
+import logo from "../images/instagram-logo.png";
+
 const Nav = () => {
-  return <div className="nav-wrapper"></div>;
+  return (
+    <div className="nav-wrapper">
+      {/* Logo */}
+      <img
+        href="#"
+        className="insta-logo"
+        src={logo}
+        alt="Instagram Logo"
+      ></img>
+
+      {/* Searchbar */}
+      <input type="search" placeholder="Search"></input>
+
+      {/* Login */}
+      <button>Log In</button>
+      <p>
+        <a href="#"> Sign Up </a>
+      </p>
+    </div>
+  );
 };
 export default Nav;
