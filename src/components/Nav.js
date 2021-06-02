917be02a8bc60e834018d2c708fdf57e4eb79cc4
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-02 15:10:18 +0200

    cnav -> size, position & logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,28 +2,27 @@ import logo from "../images/instagram-logo.png";
 
 const Nav = () => {
   return (
-    <div className="nav-wrapper">
-      {/* Logo */}
-      <img
-        href="#"
-        className="insta-logo"
-        src={logo}
-        alt="Instagram Logo"
-      ></img>
+    <div className="outer-nav-wrapper">
+      <div className="nav-wrapper">
+        {/* Logo */}
+        <a href="#">
+          <img className="insta-logo" src={logo} alt="Instagram Logo"></img>
+        </a>
 
-      {/* Searchbar */}
-      <div className="nav-searchbar">
-        {/* change later to Font Awesome */}
-        <span>🔍</span>
-        <input type="search" placeholder="Search"></input>
-      </div>
+        {/* Searchbar */}
+        <div className="nav-searchbar">
+          {/* change later to Font Awesome */}
+          <span>🔍</span>
+          <input type="search" placeholder="Search"></input>
+        </div>
 
-      {/* Login */}
-      <div className="nav-login">
-        <button>Log In</button>
-        <p>
-          <a href="#"> Sign Up </a>
-        </p>
+        {/* Login */}
+        <div className="nav-login">
+          <button>Log In</button>
+          <p>
+            <a href="#"> Sign Up </a>
+          </p>
+        </div>
       </div>
     </div>
   );
