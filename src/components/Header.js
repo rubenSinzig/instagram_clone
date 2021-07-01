5df0b3238b50f7bea39b5a5a05ae4f614ff0205c
rubenSinzig <ruben.sinzig@posteo.de>
2021-07-01 15:49:29 +0200

    start with structure for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,18 @@
+import React from "react";
+import profilePicture from "../images/profile-picture.jpg";
+
 const Header = () => {
-  return <div className="header-wrapper"></div>;
+  return (
+    <div className="header-wrapper">
+      <div className="header-wrapper-img">
+        <img src={profilePicture} alt="Mercedes-Star" />
+      </div>
+      <div className="header-wrapper-info">
+        <section className="header-info-top">top</section>
+        <section className="header-info-middle">middle</section>
+        <section className="header-info-bottom">bottom</section>
+      </div>
+    </div>
+  );
 };
 export default Header;
