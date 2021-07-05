4ad77abf3189dddd16741d435ec07688262374bd
rubenSinzig <ruben.sinzig@posteo.de>
2021-07-05 14:30:34 +0200

    SubHeader -> story-list -> structure & position

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,4 +1,36 @@
+import React from "react";
+
 const SubHeader = () => {
-  return <div className="sub-header-wrapper"></div>;
+  return (
+    <div className="sub-header-wrapper">
+      <div className="story-list-wrapper">
+        <div className="story-list-outer">
+          <div className="story-list-inner">{/* IMAGE */}</div>
+        </div>
+        <h4>Wallpaper.</h4>
+      </div>
+
+      <div className="story-list-wrapper">
+        <div className="story-list-outer">
+          <div className="story-list-inner">{/* IMAGE */}</div>
+        </div>
+        <h4>Events.</h4>
+      </div>
+
+      <div className="story-list-wrapper">
+        <div className="story-list-outer">
+          <div className="story-list-inner">{/* IMAGE */}</div>
+        </div>
+        <h4>Models.</h4>
+      </div>
+
+      <div className="story-list-wrapper">
+        <div className="story-list-outer">
+          <div className="story-list-inner">{/* IMAGE */}</div>
+        </div>
+        <h4>Shopping.</h4>
+      </div>
+    </div>
+  );
 };
 export default SubHeader;
