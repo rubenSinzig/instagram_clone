ef1f2d566d33faf345d524b571b458984505ccd1
rubenSinzig <ruben.sinzig@posteo.de>
2021-07-05 13:03:45 +0200

    header-data -> top -> insta-name, icon & button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import profilePicture from "../images/profile-picture.jpg";
+import { SiMercedes } from "react-icons/si";
 
 const Header = () => {
   return (
@@ -12,7 +13,12 @@ const Header = () => {
         </div>
       </div>
       <div className="header-wrapper-info">
-        <section className="header-info-top">top</section>
+        <section className="header-info-top">
+          <h2>mercedesbenz_classic</h2>
+          <SiMercedes className="mb-icon" />
+          <button>Follow</button>
+          <div>● ● ●</div>
+        </section>
         <section className="header-info-middle">middle</section>
         <section className="header-info-bottom">bottom</section>
       </div>
