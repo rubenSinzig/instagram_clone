7e76520f54c84c73562577f325e8c943d74f3259
rubenSinzig <ruben.sinzig@posteo.de>
2021-07-05 15:35:55 +0200

    Gallery -> Gallery Nav -> structure, icons & postions

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,34 @@
+import React from "react";
+import { BsGrid3X3 } from "react-icons/bs";
+import { GiNewspaper } from "react-icons/gi";
+import { BsCollectionPlay } from "react-icons/bs";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { BiUserPin } from "react-icons/bi";
+
 const Gallery = () => {
-  return <div className="gallery-wrapper"></div>;
+  return (
+    <div className="gallery-wrapper">
+      <div className="wrapper-gallery-link">
+        <BsGrid3X3 className="posts-icon" />
+        <h6>POSTS</h6>
+      </div>
+      <div className="wrapper-gallery-link">
+        <GiNewspaper className="guides-icon" />
+        <h6>GUIDES</h6>
+      </div>
+      <div className="wrapper-gallery-link">
+        <BsCollectionPlay className="reels-icon" />
+        <h6>REELS</h6>
+      </div>
+      <div className="wrapper-gallery-link">
+        <AiOutlineFundProjectionScreen className="igtv-icon" />
+        <h6>IGTV</h6>
+      </div>
+      <div className="wrapper-gallery-link">
+        <BiUserPin className="tagged-icon" />
+        <h6>TAGGED</h6>
+      </div>
+    </div>
+  );
 };
 export default Gallery;
