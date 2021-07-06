b8f93993b1708be30a0eadb718269324cc0a320b
rubenSinzig <ruben.sinzig@posteo.de>
2021-07-06 13:40:57 +0200

    add more keys & values to json file

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,24 +3,17 @@ import Post from "../posts.json";
 
 console.log(Post[0].image);
 
-const style = {
-  backgroundImage: `url("${Post[0].image}")`,
-};
-
 const Posts = () => {
   return (
     <div className="wrapper-posts">
-      <div>
-        {Post.map((obj, i) => {
-          return (
-            <div key={i}>
-              <h1>{obj.id}</h1>
-              <img src={`${process.env.PUBLIC_URL}/images/${obj.image}`} />
-              {console.log(`${process.env.PUBLIC_URL}/images/${obj.image}`)}
-            </div>
-          );
-        })}
-      </div>
+      {Post.map((obj, i) => {
+        return (
+          <div key={i}>
+            <img src={`${process.env.PUBLIC_URL}/images/${obj.image}`} />
+            <div className="posts-overlay"></div>
+          </div>
+        );
+      })}
     </div>
   );
 };
