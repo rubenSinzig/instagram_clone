9b2757df0c24b3702398ac3722674d73f944970e
rubenSinzig <ruben.sinzig@posteo.de>
2021-07-06 14:17:24 +0200

    hover for post gallery

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,28 @@
 import React from "react";
 import Post from "../posts.json";
+import { BsHeartFill } from "react-icons/bs";
+import { FaComment } from "react-icons/fa";
 
-console.log(Post[0].image);
-
-const Posts = () => {
+const postGallery = Post.map((obj, i) => {
   return (
-    <div className="wrapper-posts">
-      {Post.map((obj, i) => {
-        return (
-          <div key={i}>
-            <img src={`${process.env.PUBLIC_URL}/images/${obj.image}`} />
-            <div className="posts-overlay"></div>
-          </div>
-        );
-      })}
+    <div className="wrapper-posts" key={i}>
+      <img src={`${process.env.PUBLIC_URL}/images/${obj.image}`} />
+      <div className="posts-overlay">
+        <div>
+          <BsHeartFill />
+          <span>{obj.likes}</span>
+        </div>
+        <div>
+          <FaComment />
+          <span>{obj.comments}</span>
+        </div>
+      </div>
     </div>
   );
+});
+
+const Posts = () => {
+  return <div className="main-wrapper-posts">{postGallery}</div>;
 };
 
 export default Posts;
